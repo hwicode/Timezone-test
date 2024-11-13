e676518762a558b1299e6854a04a8bd0fbbfcfd7
hwicode <khc3796@naver.com>
2024-11-13 15:03:20 +0900

    프로젝트 초기화

---
+++ src/test/java/jdbc/timezone/TimezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package jdbc.timezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
